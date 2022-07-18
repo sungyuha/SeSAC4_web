5d36572a66b22ed5e2db0f6ecdea890f473421c8
sungyuha <yuha822@naver.com>
2022-07-18 16:44:06 +0900

    study: node.js

--- node.js/0718_24.js
+++ node.js/0718_24.js
@@ -1,14 +1,16 @@
-const { Console } = require("console");
 const url = require("url");
 const { URL } = url;
 const string = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=sesac&oquery=sesac&tqi=hWg%2F8sprvmZss6uZF3GssssssfV-461107"
 const naver = new URL(string);
 
-const params = new URLSearchParams('foo=bar&foo=baz');
+const params = new URLSearchParams('sm=tab_hty.top&where=nexearch&query=sesac&oquery=sesac&tqi=hWg%2F8sprvmZss6uZF3GssssssfV-461107');
 for (const name of params.keys()) {
   console.log(name);
-}
+} // 1) 위의 URL 에서 모든 키를 가져오기
 
-console.log(url.getAll(key));
+console.log( params.values() );
+// 2) 위의 URL에서 모든 값을 가져오기
 
-console.log( naver.searchParams );
+console.log( params.delete('sm') );
+// 3) 위의 URL에서 sm 키를 삭제하고 1)을 이용해 모든 키 가져오기
+console.log( params.searchParams );
--- node.js/url.js
+++ node.js/url.js
@@ -1,4 +1,3 @@
-const { Console } = require("console");
 const url = require("url");
 const { URL } = url;
 const string = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%8C%94%EC%8B%B9"
@@ -7,4 +6,4 @@ const naver = new URL(string);
 // console.log( url.format(naver) );
 console.log( url.parse(string) );
 
-console.log( naver. searchParams );
+console.log( naver.searchParams );
