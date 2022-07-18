1cbc8fcace943dab543585795b9e3f1a853438a1
sungyuha <yuha822@naver.com>
2022-07-18 17:09:38 +0900

    study: node.js

--- node.js/3_callback_hell.js
+++ node.js/3_callback_hell.js
@@ -7,17 +7,17 @@ function login(id, pw, cb) {
 
 function getVideo( id, cb  ) {
     setTimeout(() => {
-        cb(['아이언맨1'], ['아이언맨2']);
+        cb(['아이언맨1', '아이언맨2']);
     }, 2000);
 }
 
 function getDetail(Video, cb) {
     setTimeout(() => {
-        cb("비디오 제목은 : ", Video);
+        cb("비디오 제목은 : "+ Video);
     }, 1000);
 }
 
-login2('kim', '1234', user => {
+login('kim', '1234', user => {
     console.log( 'user님 환영');
     getVideo(user, (Videos) => {
         console.log( Videos );
