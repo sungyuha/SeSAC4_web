298824ec2e9c3b4b496370d8e18224140f5b720e
sungyuha <yuha822@naver.com>
2022-07-18 18:07:13 +0900

    study: node.js

--- node.js/0718_24.js
+++ node.js/0718_24.js
@@ -3,14 +3,16 @@ const { URL } = url;
 const string = "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=sesac&oquery=sesac&tqi=hWg%2F8sprvmZss6uZF3GssssssfV-461107"
 const naver = new URL(string);
 
-const params = new URLSearchParams('sm=tab_hty.top&where=nexearch&query=sesac&oquery=sesac&tqi=hWg%2F8sprvmZss6uZF3GssssssfV-461107');
+const params = new URLSearchParams(naver.search);
 for (const name of params.keys()) {
   console.log(name);
-} // 1) 위의 URL 에서 모든 키를 가져오기
+} 
+// 1) 위의 URL 에서 모든 키를 가져오기
 
 console.log( params.values() );
 // 2) 위의 URL에서 모든 값을 가져오기
 
-console.log( params.delete('sm') );
+params.delete('sm');
 // 3) 위의 URL에서 sm 키를 삭제하고 1)을 이용해 모든 키 가져오기
-console.log( params.searchParams );
+
+console.log( params.keys() );
