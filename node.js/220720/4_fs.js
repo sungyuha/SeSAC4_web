4ee3579b8fc0edbf1c7656f794720e01d61e63e4
sungyuha <yuha822@naver.com>
2022-07-20 15:40:35 +0900

    study: node.js

--- node.js/220720/4_fs.js
+++ node.js/220720/4_fs.js
@@ -1,12 +1,20 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-console.log(fs);
+// console.log(fs);
 
-fs.readFile("./test.txt", function(err, data) {
-    if (err) throw err;
+// fs.readFile("./test.txt", function(err, data) {
+//     if (err) throw err;
     
-    console.log(data);
+//     console.log(data);
 
-    console.log(data.toString());
+//     console.log(data.toString());
     
-});
+// });
+
+fs.writeFile('./write.txt', '안녕')
+    .then(() => {
+        console.log( "작성 완료" );
+    })
+    .catch((err) => {
+        console.log(err);
+    });
