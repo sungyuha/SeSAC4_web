5580be6eb00231a24b8c52e403338d6214669fea
sungyuha <yuha822@naver.com>
2022-07-25 16:51:30 +0900

    study: form

---
+++ node.js/220725/0725_32.js
@@ -0,0 +1,28 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const port = 9999;
+
+app.set("view engine", "ejs");
+app.use( express.static( "public" ));
+app.use(express.urlencoded({extended: true}));
+app.use( bodyParser.json() );
+
+app.get("/", function(req,res){
+    res.render("index_3");
+});
+
+app.get("/receive3", function(req,res){
+    console.log( req.query );
+    res.render("receive3", req.query);
+});
+
+app.post("/receive3", function(req,res){
+    console.log("receive3-post");
+    console.log( req.body );
+    res.render("receive3", req.body);
+});
+
+app.listen(port, ()=>{
+    console.log( "Server Port :", port );
+});
