ae39f533613c4abb9a869c62d1b7fd1a1eaf691b
sungyuha <yuha822@naver.com>
2022-07-27 15:40:35 +0900

    study: axios

---
+++ node.js/220727/0727_33.js
@@ -0,0 +1,27 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const port = 9999;
+
+app.set("view engine", "ejs");
+app.use( express.static( "public" ));
+app.use(express.urlencoded({extended: true}));
+app.use( bodyParser.json() );
+
+app.get("/", function(req,res){
+    res.render("index2");
+});
+
+app.get("/receive2", function(req,res){
+    console.log( req.query );
+    res.render("receive2", req.query);
+});
+
+app.post("/receive2", function(req,res){
+    console.log( req.body );
+    res.render("receive2", req.body);
+});
+
+app.listen(port, ()=>{
+    console.log( "Server Port :", port );
+});
