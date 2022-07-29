d3f59a501e416c66b5087e5d307d800e47964993
sungyuha <yuha822@naver.com>
2022-07-29 17:02:39 +0900

    study: nodejs and multer

--- node.js/220727/0727_34.js
+++ node.js/220727/0727_34.js
@@ -1,48 +1,28 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 9999;
-const fs = require("fs").promises;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
 app.use( express.static( "public" ));
-app.use(express.urlencoded({extended: true}));
+app.use( express.urlencoded({extended: true}));
 app.use( bodyParser.json() );
-
-app.get("/", function(req,res){
-    res.render("index3");
-});
 
 app.get("/receive3", function(req,res){
     console.log( req.query );
     res.render("receive3", req.query);
 });
 
-// app.post("/receive3", function(req,res){
-//     fs.readFile("./public/info.txt")
-//     .then((data) => {
-//         let arr = data.toString().split("//");
-//         if ( arr[0] == req.body.id && arr[1] == req.body.pw ){
-//            res.rander("로그인이 되었습니다.");
-//         } else {
-//             res.rander("로그인이 실패하였습니다.");
-//         }
-// });
-
-    // arr = { "sesac", "sesac1234" };
-
-    app.post("/receive3", function(req,res){
-        if (err) throw err;
-        data = ("./public/info.txt");
-        console.log(data);
-        
-        console.log(data.toString().split("//")); 
-        if ( arr[0] == req.body.id && arr[1] == req.body.pw ){
-            res.rander("로그인이 되었습니다.");
-         } else {
-             res.rander("로그인이 실패하였습니다.");
-         }    
-        //callback 함수 
+app.post("/receive3", function(req,res){
+        fs.readFile("./public/info.txt")
+        .then((data) => {
+            let arr = data.toString().split("//");
+            if ( arr[0] == req.body.id && arr[1] == req.body.pw ){
+               res.rander("로그인이 되었습니다.");
+            } else {
+                res.rander("로그인이 실패하였습니다.");
+            }
+    });
 });
 
 app.listen(port, ()=>{
