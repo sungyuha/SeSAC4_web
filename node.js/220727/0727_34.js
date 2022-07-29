b5898d3bfe6e819fd5c95c408d5489b259c0e087
sungyuha <yuha822@naver.com>
2022-07-29 17:22:29 +0900

    study: nodejs and express

--- node.js/220727/0727_34.js
+++ node.js/220727/0727_34.js
@@ -2,25 +2,30 @@ const express = require("express");
 const app = express();
 const port = 9999;
 const bodyParser = require("body-parser");
+const fs = require("fs").promises;
 
 app.set("view engine", "ejs");
 app.use( express.static( "public" ));
 app.use( express.urlencoded({extended: true}));
 app.use( bodyParser.json() );
 
-app.get("/receive3", function(req,res){
-    console.log( req.query );
-    res.render("receive3", req.query);
+app.get("/", function(req,res){
+    res.render("index4");
 });
 
-app.post("/receive3", function(req,res){
+app.get("/receive4", function(req,res){
+    console.log( req.query );
+    res.send("receive4", req.query);
+});
+
+app.post("/receive4", function(req,res){
         fs.readFile("./public/info.txt")
         .then((data) => {
             let arr = data.toString().split("//");
             if ( arr[0] == req.body.id && arr[1] == req.body.pw ){
-               res.rander("로그인이 되었습니다.");
+               res.send("로그인이 되었습니다.");
             } else {
-                res.rander("로그인이 실패하였습니다.");
+                res.send("아이디와 비밀번호를 확인해주세요.");
             }
     });
 });
