3abbee14d6b09b6936447439ff1a5584795f3fb6
sungyuha <yuha822@naver.com>
2022-07-29 16:41:15 +0900

    stduy: nodejs and multer

--- node.js/220729/0729_36.js
+++ node.js/220729/0729_36.js
@@ -4,33 +4,37 @@ const port = 9999;
 const bodyParser = require("body-parser");
 const multer = require("multer");
 const path = require("path");
+
+app.set("view engine", "ejs");
+app.use( express.static( "uploads" ));
+app.use( express.urlencoded({extended: true}));
+app.use( bodyParser.json() );
+
+// const { futimesSync } = require("fs");
 const upload = multer({
     // dest: "uploads/" // 파일을 올리겠다, 파일 경로를 정해줌
     storage: multer.diskStorage({
         destination(req, file, done){
+            console.log("destination : ", req.body );
             done( null, 'uploads/');
         },
         filename( req, file, done) {
             const ext = path.extname(file.originalname);
-            done(null, path.basename(file.originalname, ext) + ext );
+            done(null, req.body.id + ext );
         },
     }),
     limits: { fileSize: 5*1024*1024 },
 });
-
-app.set("view engine", "ejs");
-app.use( express.static( "public" ));
-app.use( express.urlencoded({extended: true}));
-app.use( bodyParser.json() );
-app.use( '/aa', express.static( 'uploads' ));
-
 app.get("/", function(req, res){
     res.render("index2");
 });
+app.get("/upload", function(req, res){
+    res.render("upload");
+});
 app.post("/upload", upload.single('userfile'), function(req,res){
+    res.render("upload-page", {filename: req.file.filename});
     console.log(req.body);
     console.log(req.file);
-    res.render("upload-page", res.file);
 });
 
 app.listen(port, ()=> {
