8b6019ce1e1ec1afcaa401da205d9f3cc66f3ca8
sungyuha <yuha822@naver.com>
2022-08-05 18:32:24 +0900

    study: node.js and MVC

---
+++ node.js/220805/routes/posts.js
@@ -0,0 +1,7 @@
+// router.get("/:shortId", async (req, res, next) => {
+    
+//     if (req.query.edit) { // query가 edit으로 들어왔을 때
+//       res.render("posts/edit", { post }); // 아까 상세 페이지에서 찾았던 post값 넘겨주기
+//     }
+    
+//   });
