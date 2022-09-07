8cdd817ab730f55c49dd89c87ea91efa09295c4e
sungyuha <yuha822@naver.com>
2022-09-07 17:26:49 +0900

    socket chat css

--- node.js/220907/chat.js
+++ node.js/220907/chat.js
@@ -10,13 +10,19 @@ app.get("/", function(req, res){
 
 // io -> 클라이언트와의 모든 연결을 갖고 있는 친구
 // socket -> 클라이언트 한 명. socket.id 클라이언트를 구분하는 식별자
+// connection는 정해져있는 것
 io.on("connection", function(socket){   
     console.log( "connected : ", socket.id ); // socket.id로 누가 누구인지 구분 가능
     socket.emit("info", socket.id); // info로 socket.id가 저장 됨
+    io.emit("notice", socket.id + "님이 입장하셨습니다.");
     socket.on("send", function(data){
         // 넘어온 data = {id: ~~~, msg: ~~~};
         console.log( "client message : ", data.msg ); // 내가 console.log를 찍을 때 client message
         io.emit("newMessage", data );
+    });
+
+    socket.on("disconnent", function(){ // disconnent은 연결이 끊겼을 때 실행 됨
+        io.emit("notice", socket.id + "님이 퇴장하셨습니다." );
     });
 });
 
