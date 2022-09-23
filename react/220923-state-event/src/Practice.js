162367514444c128af074ebbba3ce9da8aa63d43
sungyuha <yuha822@naver.com>
2022-09-23 18:05:06 +0900

    study: React Component

--- react/220923-state-event/src/Practice.js
+++ react/220923-state-event/src/Practice.js
@@ -2,27 +2,21 @@ import React, { useState } from 'react';
 
 // 실습 57
 function Practice(){
-    const [num, setNum] = useState("");
+    const [num, setNum] = useState(0);
 
     function Increase(){
-        
+        setNum(num+2);
     }
 
     function Decrease(){
-        
+        setNum(num-1);
     }
 
     return (
         <div>
             <h1>숫자 : {num}</h1>
-            <button 
-                onClick={Increase}>
-                    +2
-            </button>
-            <button 
-                onClick={Decrease}>
-                    -1
-            </button>
+            <button onClick={Increase}>+2</button>
+            <button onClick={Decrease}>-1</button>
         </div>
     );
 }
