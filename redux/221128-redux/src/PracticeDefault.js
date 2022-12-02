e03d9ad227115ecc9437eedf16fbe9ece5807e7a
sungyuha <yuha822@naver.com>
2022-12-02 16:16:36 +0900

    docker

--- redux/221128-redux/src/PracticeDefault.js
+++ redux/221128-redux/src/PracticeDefault.js
@@ -3,7 +3,14 @@ import { createStore } from 'redux';
 import { Provider, useSelector, useDispatch } from 'react-redux';
 
 function reducer(state, action) {
-  
+  // state : 현재 상태의 state -> 아직 바뀌기 전
+  // action : dispatch를 실행할 때 넣었던 객체
+  if ( action.type === 'INCREASE' ){
+    return { number: state.number + 1 };
+  } else if ( action.type === 'DECREASE' ){
+    return { number: state.number - 1}
+  }
+  return { number: 1};
 }
 
 const store = createStore(reducer);
